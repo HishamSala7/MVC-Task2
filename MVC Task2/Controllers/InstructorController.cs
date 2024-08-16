c431fb325f2d39c40bce29c004a04deee3d20896
Hisham Salah <Hisham Salah@DESKTOP-BCIHSE4>
2024-08-16 15:52:02 +0300

    Add project files.

---
+++ MVC Task2/Controllers/InstructorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Task2.Models;
+
+namespace MVC_Task2.Controllers
+{
+	public class InstructorController : Controller
+	{
+		WebAppContext context = new WebAppContext();
+		public IActionResult GetAllInstructors()
+		{
+			List<Instructor> InstructorList = context.Instructors.ToList();
+			return View("GetAllInstructor",InstructorList);
+		}
+
+		public IActionResult Details(int id)
+		{
+			Instructor instructor = context.Instructors.FirstOrDefault(x=>x.Id == id);
+			return View("Details", instructor);
+		}
+	}
+}
